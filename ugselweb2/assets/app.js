a7d8869ddfe540123f58562f595af688d8d70b77
Noobos100 <charles.daw@etu.univ-amu.fr>
2025-02-11 14:41:34 +0100

    Add logos folder

--- ugselweb2/assets/app.js
+++ ugselweb2/assets/app.js
@@ -7,5 +7,6 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 import './styles/home.css';
+import './logos/logoun.png';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
