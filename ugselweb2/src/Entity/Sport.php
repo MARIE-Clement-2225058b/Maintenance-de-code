142ee0a7224c76a8b6725a09ef7dac4c7e73a70d
Noobos100 <charles.daw@etu.univ-amu.fr>
2025-02-16 21:45:14 +0100

    add sport entity

---
+++ ugselweb2/src/Entity/Sport.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SportRepository::class)]
+class Sport
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    /**
+     * @var int|null
+     * The UID of the sport
+     */
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    /**
+     * @var string|null
+     * The name of the sport
+     */
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
